f1d31f58dee0f1316687c3d21dd6adbb2a6f16d9
agusann <agustinannese1@gmail.com>
2025-06-26 23:48:55 -0300

    punto2.pas

--- PTRACTICA 3/punto2.pas
+++ PTRACTICA 3/punto2.pas
@@ -1,12 +1,18 @@
 program punt2;
 type
+	fechas=record
+		dia:integer;
+		mes:integer;
+		anio:integer;
+	end;
+	
     venta=record
         cod:integer;
         fecha:fechas;
         cant:integer;
     end;
 
-    arbol=ˆnodo;
+    arbol=^nodo;
     nodo=record
         dato:venta;
         HI:arbol;
@@ -18,19 +24,36 @@ type
         begin
             if (a = nil) then begin
                 new (a);
-                aˆ.dato:=v
-                aˆ.HI:=nil;
-                aˆ.HD:=nil;
+                a^.dato:=v;
+                a^.HI:=nil;
+                a^.HD:=nil;
             end
             else
-                if (v.cod < aˆ.dato.cod) then begin
-                    CREAR_NODO (Aˆ.HI,v);
+                if (v.cod < a^.dato.cod) then begin
+                    CREAR_NODO (a^.HI,v);
                 end
                 else
-                    CREAR_NODO (Aˆ.HD,v);
+                    CREAR_NODO (a^.HD,v);
         end;
     var
         v:venta;
     begin
-        
+        v.cod:= RANDOM (100);
+        if (v.cod <> 0) then begin
+			v.fecha.dia:= 1 + RANDOM (31);
+			v.fecha.mes:= 1 + RANDOM (12);
+			v.fecha.anio:= 2000 + RANDOM (26);
+			v.cant:= RANDOM (400);
+        end;
+        while (v.cod <> 0) do begin
+			CREAR_NODO(a,v);
+			v.cod:= RANDOM (100);
+			if (v.cod <> 0) then begin
+				v.fecha.dia:= 1 + RANDOM (31);
+				v.fecha.mes:= 1 + RANDOM (12);
+				v.fecha.anio:= 2000 + RANDOM (26);
+				v.cant:= RANDOM (400);
+			end;	
+        end;
     end;
+
